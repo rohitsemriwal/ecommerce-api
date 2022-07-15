d2d62baef64ed170818e014cf17cc18018b46322
Rohit Semriwal <rohitsemriwal@gmail.com>
2022-07-15 19:43:30 +0530

    Ep.6

--- src/routes/file_routes.js
+++ src/routes/file_routes.js
@@ -12,4 +12,21 @@ router.post("/single", upload.single('image'), async function(req, res) {
     res.json({ success: true, data: "http://localhost:5000/" + uploadedFile.filename });
 });
 
+router.post("/multiple", upload.array('images', 10), async function(req, res) {
+    const uploadedFiles = req.files;
+
+    if(!uploadedFiles || uploadedFiles.length == 0) {
+        res.json({ success: false, error: "files-not-uploaded" });
+        return;
+    }
+
+    var downloadurls = [];
+    uploadedFiles.forEach(function(uploadedFile) {
+        const downloadurl = "http://localhost:5000/" + uploadedFile.filename;
+        downloadurls.push(downloadurl);
+    });
+
+    res.json({ success: true, data: downloadurls });
+});
+
 module.exports = router;
